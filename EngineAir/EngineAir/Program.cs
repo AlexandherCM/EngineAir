87f12eeb9039d951238aad7b2ddad48a0bfa25a3
BrayanEstevezC <brayan_1321124024@uptecamac.edu.mx>
2024-02-20 03:01:54 -0600

    CHB_0017_FormsComplete

--- EngineAir/EngineAir/Program.cs
+++ EngineAir/EngineAir/Program.cs
@@ -53,6 +53,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Componentes}/{action=Motor}/{id?}");
+    pattern: "{controller=Componentes}/{action=Variante}/{id?}");
 
 app.Run();
